5fc7efb4541782119bb7e3becab20a623eb2328c
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-07-31 07:28:00 -0500

    feat: build home page header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,26 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 export default function Home() {
+  const loggedIn = { firstName: "Kamsiyonna" };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to Clivo Financial</h1>
-    </main>
+    <section className="home">
+      <div className="home-content">
+        <header>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            subtext="Access and manage your account and transactions efficiently"
+            user={loggedIn?.firstName || "Guest"}
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1500}
+          />
+        </header>
+      </div>
+    </section>
   );
 }
